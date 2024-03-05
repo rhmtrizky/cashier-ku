f1a3f89352d2daa96c1d3402675c2d878380cb78
rhmtrizky <rahmatrizkyrifai@gmail.com>
2024-03-05 23:03:28 +0700

    Completed feat search product and store data products

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row>
-    <v-col cols="8" class="frame-container px-6">
-      <products></products>
+    <v-col cols="8" class="frame-container px-3">
+      <product></product>
     </v-col>
     <v-col cols="4" class="frame-container white px-6">Orders</v-col>
   </v-row>
